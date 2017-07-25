9c37516e1e6548c1c8418d4548414edb77316cbf
Alexandr Nikitin <nikitin.alexandr.a@gmail.com>
2017-07-25 16:55:26 +0300

    Resources to Sandbox

---
+++ sandbox/BKTree.NET.Sandbox/Utils/ResourcesUtils.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+
+namespace BKTree.NET.Sandbox.Utils
+{
+    public static class ResourcesUtils
+    {
+        public static IEnumerable<string> Get(string resourceName)
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+
+            using (var stream = assembly.GetManifestResourceStream(resourceName))
+            using (var reader = new StreamReader(stream))
+            {
+                while (!reader.EndOfStream)
+                {
+                    yield return reader.ReadLine();
+                }
+            }
+        }
+    }
+}
